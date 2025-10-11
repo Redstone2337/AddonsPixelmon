dee6515311ff37523786d0230dc27ad676f5bd77
Redstone233 <3356168312@qq.com>
2025-10-12 07:23:26 +0800

    Initial commit

--- src/main/java/net/redstone233/pixelmonaddon/AddonsPixelmonClient.java
+++ src/main/java/net/redstone233/pixelmonaddon/AddonsPixelmonClient.java
@@ -11,15 +11,15 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = AddonsPixelmon.MODID, dist = Dist.CLIENT)
+@Mod(value = AddonsPixelmon.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = AddonsPixelmon.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = AddonsPixelmon.MOD_ID, value = Dist.CLIENT)
 public class AddonsPixelmonClient {
     public AddonsPixelmonClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
         // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
         // Do not forget to add translations for your config options to the en_us.json file.
-        //container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 
     @SubscribeEvent
